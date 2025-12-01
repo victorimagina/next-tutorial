ee474cbfab91148ab5af4d0f2ae891337f0f5f9a
Víctor Martínez Zapata <victor@imaginagroup.com>
2025-12-01 16:11:43 +0100

    Tutorial completado

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <title>Nexjs dashboard</title>
+      <meta name="description" content="This is a example of a nextjs dashboard" />
+      <meta name="keywords" content="keyword1, keyword2, keyword3" />
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
